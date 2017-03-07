35d7d97b81f42b5bda32638682a1764b7d67e4ba
Paola Claros <paolita.claros@gmail.com>
2017-03-06 17:04:14 -0800

    add post, patch and delete

--- routes/books.js
+++ routes/books.js
@@ -31,7 +31,7 @@ router.get('/books/:id', (req, res, next) => {
     });
 });
 
-router.post('books', (req, res, next) => {
+router.post('/books', (req, res, next) => {
   knex('books')
     .insert({
       title: req.body.title,
@@ -39,12 +39,61 @@ router.post('books', (req, res, next) => {
       genre: req.body.genre,
       description: req.body.description,
       cover_url: req.body.cover_url
-    })
-    .then((book) => {
-      res.send(book);
+    }, '*')
+    .then((books) => {
+      res.send(books[0]);
     })
     .catch((err) => {
       next(err);
     });
+});
+
+router.patch('/books/:id', (req, res, next) => {
+  knex('books')
+  .where('id', req.params.id)
+  .first()
+  .then((book) => {
+    if (!book) {
+      return next();
+    }
+    return knex('books')
+    .update({
+      title: req.body.title,
+      author: req.body.author,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.cover_url
+    }, '*')
+    .where('id', req.params.id)
+  })
+  .then((books) => {
+    res.send(books[0]);
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.delete('/books/:id', (req, res, next) => {
+  let book;
+  knex('books')
+  .where('id', req.params.id)
+  .first()
+  .then((row) => {
+    if (!row) {
+      return next();
+    }
+    book = row;
+    return knex('books')
+    .del()
+    .where('id', req.params.id)
+  })
+  .then(() => {
+    delete book.id;
+    res.send(book);
+  })
+  .catch((err) => {
+    next(err);
+  });
 });
 module.exports = router;
