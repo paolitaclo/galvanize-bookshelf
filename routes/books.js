69a182236d54a6566b5478b7abdad3570875fbca
Paola Claros <paolita.claros@gmail.com>
2017-03-12 00:48:58 -0800

    new changes to book.js on routes folder

--- routes/books.js
+++ routes/books.js
@@ -5,100 +5,99 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
-router.get('/books', (req, res, next) => {
-  knex('books')
-    .orderBy('title')
+router.route('/books').
+  get((req, res, next) => {
+    knex('books')
+      .orderBy('title')
+      .then((books) => {
+        res.send(books);
+      })
+      .catch((err) => {
+        res.sendStatus(404);
+        next(err);
+      });
+  })
+  .post((req, res, next) => {
+    knex('books')
+      .insert({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        description: req.body.description,
+        cover_url: req.body.cover_url
+      }, '*')
+      .then((books) => {
+        res.send(books[0]);
+      })
+      .catch((err) => {
+        res.sendStatus(400);
+        next(err);
+      });
+  });
+
+router.route('/books/:id')
+  .get((req, res, next) => {
+    knex('books')
+      .where('id', req.params.id)
+      .first()
+      .then((book) => {
+        if (!book || req.params.id < 0 || Number.isNaN(req.params.id)) {
+          return next();
+        }
+        res.send(book);
+      })
+      .catch((err) => {
+        res.sendStatus(404);
+        next(err);
+      });
+  })
+  .patch((req, res, next) => {
+    knex('books')
+    .where('id', req.params.id)
+    .first()
+    .then((book) => {
+      if (!book) {
+        return next();
+      }
+      return knex('books')
+      .update({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        description: req.body.description,
+        cover_url: req.body.cover_url
+      }, '*')
+      .where('id', req.params.id)
+    })
     .then((books) => {
-      res.send(books);
+      res.send(books[0]);
     })
     .catch((err) => {
       res.sendStatus(404);
       next(err);
     });
-});
-
-router.get('/books/:id', (req, res, next) => {
-  knex('books')
+  })
+  .delete((req, res, next) => {
+    let book;
+    knex('books')
     .where('id', req.params.id)
     .first()
-    .then((book) => {
-      if (!book || req.params.id < 0 || Number.isNaN(req.params.id)) {
+    .then((row) => {
+      if (!row) {
         return next();
       }
+      book = row;
+      return knex('books')
+      .del()
+      .where('id', req.params.id)
+    })
+    .then(() => {
+      delete book.id;
       res.send(book);
     })
     .catch((err) => {
       res.sendStatus(404);
       next(err);
     });
-});
-
-router.post('/books', (req, res, next) => {
-  knex('books')
-    .insert({
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      description: req.body.description,
-      cover_url: req.body.cover_url
-    }, '*')
-    .then((books) => {
-      res.send(books[0]);
-    })
-    .catch((err) => {
-      res.sendStatus(400);
-      next(err);
-    });
-});
-
-router.patch('/books/:id', (req, res, next) => {
-  knex('books')
-  .where('id', req.params.id)
-  .first()
-  .then((book) => {
-    if (!book) {
-      return next();
-    }
-    return knex('books')
-    .update({
-      title: req.body.title,
-      author: req.body.author,
-      genre: req.body.genre,
-      description: req.body.description,
-      cover_url: req.body.cover_url
-    }, '*')
-    .where('id', req.params.id)
-  })
-  .then((books) => {
-    res.send(books[0]);
-  })
-  .catch((err) => {
-    res.sendStatus(404);
-    next(err);
   });
-});
-
-router.delete('/books/:id', (req, res, next) => {
-  let book;
-  knex('books')
-  .where('id', req.params.id)
-  .first()
-  .then((row) => {
-    if (!row) {
-      return next();
-    }
-    book = row;
-    return knex('books')
-    .del()
-    .where('id', req.params.id)
-  })
-  .then(() => {
-    delete book.id;
-    res.send(book);
-  })
-  .catch((err) => {
-    res.sendStatus(404);
-    next(err);
-  });
-});
 module.exports = router;
--- routes/favorites.js
+++ routes/favorites.js
@@ -4,7 +4,68 @@ const express = require('express');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
+const knex = require('../knex');
+const { camelizeKeys, decamelizeKeys } = require('humps');
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt-as-promised');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
 
-// YOUR CODE HERE
+router.route('/favorites')
+  .get((req, res, next) => {
+    if (!req.cookies.token) {
+      res.set('Content-Type', 'text/plain');
+      res.status(401).send('Unauthorized');
+    }
+    else {
+      return knex('favorites')
+      .orderBy('author')
+      .then((favorites) => {
+        console.log(favorites);
+        res.set('Content-Type', /json/);
+        res.send(camelizeKeys(favorites));
+      })
+      .catch((err) => {
+        next();
+      });
+    }
+  })
+  .post((req, res, next) => {
+    if (!req.cookies.token) {
+      res.set('Content-Type', 'text/plain');
+      res.status(401).send('Unauthorized');
+    }
+    else {
+      return knex('favorites')
+      .orderBy('author')
+      .then((favorites) => {
+        console.log(favorites);
+        res.set('Content-Type', /json/);
+        res.send(camelizeKeys(favorites));
+      })
+      .catch((err) => {
+        next();
+      });
+    }
+  })
+  .delete((req, res, next) => {
+    if (!req.cookies.token) {
+      res.set('Content-Type', 'text/plain');
+      res.status(401).send('Unauthorized');
+    }
+    else {
+      return knex('favorites')
+      .orderBy('author')
+      .then((favorites) => {
+        console.log(favorites);
+        res.set('Content-Type', /json/);
+        res.send(camelizeKeys(favorites));
+      })
+      .catch((err) => {
+        next();
+      });
+    }
+  })
 
 module.exports = router;
