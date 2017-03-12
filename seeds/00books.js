ff6011b86cd9c89e37411d6526d0f8740d0b9b0c
Paola Claros <paolita.claros@gmail.com>
2017-03-11 23:50:44 -0800

    create seed favorite seed

--- seeds/00books.js
+++ seeds/00books.js
@@ -1,10 +1,7 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('books').del()
-  .then(function () {
-    // return Promise.all([
-      // Inserts seed entries
+  .then(() => {
     return knex('books').insert([
       {
         id: 1,
