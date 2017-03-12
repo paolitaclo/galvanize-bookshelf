ba96235823c11596319fce70a08a04e591a574e9
Paola Claros <paolita.claros@gmail.com>
2017-03-11 20:41:48 -0800

    add functionality to pass  bonus routes.token tests

--- routes/token.js
+++ routes/token.js
@@ -12,34 +12,49 @@ const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
 
 router.route('/token')
-  .get((req, res, next) => {
+  .get((req, res) => {
     res.set('Content-Type', 'application/json');
     res.status(200).send('false');
   })
-  .post((req, res, next) => {
-    return knex('users').where('email', req.body.email)
-    .then((users) => {
-      return bcrypt.compare(req.body.password, users[0].hashed_password)
-      .then((response) => {
-          return response;
-      })
-      .then((userAuth) => {
-        console.log(userAuth);
-        delete users[0].hashed_password;
-        let claims = {
-          sub: users[0].id,
-          iss: 'https://localhost:8000'
-        };
-        let token = jwt.sign(claims, process.env.JWT_KEY);
-        res.cookie('token', token, { path: '/', httpOnly: true });
-        res.send(camelizeKeys(users[0]));
-      })
-      .catch((err) => {
-        res.set('Content-Type', 'text/plain');
-        res.status(400).send('Bad email or password');
-        next(err);
+  .post((req, res) => {
+    if (!req.body.email) {
+      res.set('Content-Type', 'text/plain');
+      res.status(400).send('Email must not be blank');
+    }
+    else if (!req.body.password) {
+      res.set('Content-Type', 'text/plain');
+      res.status(400).send('Password must not be blank');
+    }
+    else {
+      return knex('users').where('email', req.body.email)
+      .then((users) => {
+        if (!users[0]) {
+          res.set('Content-Type', 'text/plain');
+          res.status(400).send('Bad email or password');
+        }
+        return bcrypt.compare(req.body.password, users[0].hashed_password)
+        .then((response) => {
+            return response;
+        })
+        .catch((err) => {
+          res.set('Content-Type', 'text/plain');
+          res.status(400).send('Bad email or password');
+        })
+        .then((userAuth) => {
+          delete users[0].hashed_password;
+          let claims = {
+            sub: users[0].id,
+            iss: 'https://localhost:8000'
+          };
+          let token = jwt.sign(claims, process.env.JWT_KEY);
+          res.cookie('token', token, { path: '/', httpOnly: true });
+          res.send(camelizeKeys(users[0]));
+        })
+        .catch((err) => {
+          next(err);
+        });
       });
-    });
+    }
   });
   // .post((req, res, next) => {
   //     if(req.body.pasword < 0 || !req.body.password) {
